7a8da092bbdf483a4866ecdf46f16e01bfff316f
Andreas Lindeman <andreas.e.lindeman@gmail.com>
2026-03-05 15:54:19 +0100

    Added simple processing

--- app/routes/manual_tests/analyse.py
+++ app/routes/manual_tests/analyse.py
@@ -1,8 +1,28 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request, jsonify
 from app.socket_events import get_active_slaves_count
+from app.utils.processor import process_image
 
 analyse_bp = Blueprint("analyse", __name__)
 
 @analyse_bp.route("/analyse")
 def analyse():
     return render_template("analyse.html", slave_count = get_active_slaves_count())
+
+
+@analyse_bp.route('/upload-and-process', methods=['POST'])
+def handle_upload():
+    data = request.get_json()
+    if not data or 'media' not in data:
+        return jsonify({"error": "Ingen data mottatt"}), 400
+
+    media_data = data['media']
+    file_type = data.get('filetype', 'unknown')
+
+    # This calls the background socket logic to talk to slaves
+    print(f"Sending {file_type} to processing stations...")
+    result = process_image(media_data)
+    
+    return jsonify({
+        "status": "success",
+        "slave_output": result
+    })
