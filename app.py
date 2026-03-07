e25a3239fe326e08341ce9d276b26537b2e7a832
Andreas Lindeman <andreas.e.lindeman@gmail.com>
2026-03-07 19:28:24 +0100

    Moved max content limit to socketio

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import app.db
 from app.extensions import socketio
 
 app = create_app()
-app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024 # 64 Megabytes
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, port=5555)
