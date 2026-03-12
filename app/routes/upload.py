f745352b5068ffbbaf4e2d74fa1aeeb3150e1e7e
Chello <christinbertelsen@gmail.com>
2026-03-12 13:11:36 +0100

    skrivefeil

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -166,11 +166,11 @@ def upload():
     #"extract" verdiene:
     pi_id = pkg["pi_id"]
     ts = pkg["ts"]
-    sensor_values = pkg["sensor_value"]
+    sensor_value = pkg["sensor_value"]
     depth = pkg["depth"]
 
     #legger til i database basert på typen på instance
-    for value in sensor_values.items():
+    for value in sensor_value.items():
         if isinstance(value, dict):
             for timeStamp, sensorValue in value.items():
                 add_to_database(pi_id, timeStamp, sensorValue, depth)
