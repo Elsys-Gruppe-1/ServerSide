ea4c2e0121ea6837eb89519cf947f4d57dba2887
Chello <christinbertelsen@gmail.com>
2026-04-16 11:22:04 +0200

    ryddet i sensor.py + la til knapp for å laste ned data

--- app/routes/sensor.py
+++ app/routes/sensor.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, redirect, url_for, render_template, jsonify
-from .upload import get_data
+from flask import Blueprint, render_template, jsonify, send_file
+from app.db import Session, Measurements
+from io import StringIO, BytesIO
+import csv
 
 sensor_bp = Blueprint("sensor", __name__)
 
@@ -7,6 +9,47 @@ sensor_bp = Blueprint("sensor", __name__)
 def sensor():
     return render_template("sensor.html")
 
+# Til grafene
+def get_data():
+    with Session() as session:
+        measurements = session.query(Measurements).all()
+        result = []
+    
+        for m in measurements:
+            result.append({"pi_id":m.pi_id,
+                        "sensor_name":m.sensor_name,
+                        "ts":m.ts,
+                        "sensor_value":m.sensor_value,
+                        "depth":m.depth})
+        return result
+
 @sensor_bp.route("/api/data")
 def api_data():
     return jsonify(get_data())
+
+
+def csv_download():
+    with Session() as session:
+        measurements = session.query(Measurements).all()
+    
+    output = StringIO()
+    writer = csv.writer(output)
+    writer.writerow(["pi_id", "sensor_name", "ts", "sensor_value", "depth"])
+
+    for m in measurements:
+        writer.writerow([m.pi_id, m.sensor_name, m.ts, m.sensor_value, m.depth])
+    
+    memory_file = BytesIO()
+    memory_file.write(output.getvalue().encode("utf-8"))
+    memory_file.seek(0)
+
+    return send_file(
+        memory_file,
+        mimetype="text/csv",
+        as_attachment=True,
+        download_name="sensor_data.csv"
+    )
+
+@sensor_bp.route("/api/download")
+def download_data():
+    return csv_download()
