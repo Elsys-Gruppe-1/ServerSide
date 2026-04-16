0a86d4281cb301521f8ff874bc15481f32f71668
Chello <christinbertelsen@gmail.com>
2026-04-16 11:45:58 +0200

    skrivefeil

--- app/routes/sensor.py
+++ app/routes/sensor.py
@@ -42,7 +42,7 @@ def csv_download():
             readable_ts = datetime.fromtimestamp(m.ts). strftime("%Y-%m-%d %H:%M-%S")
         else:
             readable_ts = m.ts
-        writer.writerow([m.pi_id, m.sensor_name, m.readable_ts, m.sensor_value, m.depth])
+        writer.writerow([m.pi_id, m.sensor_name, readable_ts, m.sensor_value, m.depth])
     
     memory_file = BytesIO()
     memory_file.write(output.getvalue().encode("utf-8"))
